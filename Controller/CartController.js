4ca1e0c7e0a3bdb3699b23f0778c3b9127be4197
vipin <vipinkodalikuth@gmail.com>
2023-06-10 20:17:29 +0530

    cartlist

--- Controller/CartController.js
+++ Controller/CartController.js
@@ -5,26 +5,32 @@ const CartItem = async (req, res) => {
   try {
     const { user, cartItem, subtotal } = req.body;
     console.log(cartItem);
-    console.log(user);
-    const cartlist = await CartModel.findOne({ user: user });
-    // if(cartitem){
-    //   const cartItem = await CartModel.findOneAndUpdate({user:user});
-    // }
-    console.log(cartlist, "cartitemssss");
-    const cart = await CartModel.create({
-      user,
-      cartItem,
-      subtotal,
-    });
-    if (cart) {
-      res.json(cart);
-    } else {
-      res.json({ message: "Failed to create cart item" });
+    console.log(user,'userrrrrrrrrrr');
+    const cartlist = await CartModel.findOne({user });
+    if(cartlist){
+      cartlist.cartItem=cartItem;
+      cartlist.subtotal=subtotal;
+      await cartlist.save()
+    }else{
+      const cart = await CartModel.create({
+        user,
+        cartItem,
+        subtotal,
+      });
+      if (cart) {
+        res.json(cart);
+      } else {
+        res.json({ message: "Failed to create cart item" });
+      }
     }
+   
   } catch (error) {
     res.send({ message: "Internal server error" });
   }
 };
+
+
+
 const getCartItems = async (req, res) => {
   try {
     const cart = await CartModel.find(req.params.id);
